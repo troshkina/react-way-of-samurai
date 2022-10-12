af10754ce61dfa016e53a9d097aeaaed1424e1f2
Vladislav Kozlovsky <vkozlovsky@atypicaldigital.com>
2022-10-12 22:21:18 +0300

    refactor

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,23 +12,36 @@ const Message = (props) => {
     return <div className={s.dialog}>{props.message}</div>
 }
 
-const Dialogs = (props) => {
+const Dialogs = () => {
+
+    let dialogs = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    let messages = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'}
+    ]
+    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messages.map ( m => <Message message={m.message} />);
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Dimych" id="1"/>
-                <DialogItem name="Andrey" id="2"/>
-                <DialogItem name="Sveta" id="3"/>
-                <DialogItem name="Sasha" id="4"/>
-                <DialogItem name="Viktor" id="5"/>
-                <DialogItem name="Valera" id="6"/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message="Hi"/>
-                <Message message="How is your it-kamasutra"/>
-                <Message message="Yo"/>
-                <Message message="Yo"/>
-                <Message message="Yo"/>
+                {messagesElements}
             </div>
         </div>
     )
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -3,6 +3,16 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let posts = [
+        {id: 1, message: 'Hi, how are you', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+
+    ]
+
+    let postsElements =
+        posts.map ( p => <Post message={p.message} likesCount={p.likesCount}/>)
+
     return <div className={s.postBlock}>
         <h3> My posts </h3>
         <div>
@@ -14,8 +24,7 @@ const MyPosts = () => {
             </div>
         </div>
         <div className={s.posts}>
-            <Post message='Hi, how are you?'/>
-            <Post message="It's my first post"/>
+            {postsElements}
         </div>
     </div>
 }
